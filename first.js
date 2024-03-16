ba13cde373a14b8ba442a385d0596289238bd7da
asim424 <asimshrivastava424.com>
2024-03-16 12:16:58 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,36 @@
+console.log("Jai Gurudev");
+
+const student = {
+    fullName : "Ayushi Shrivatsava",
+    age : 24,
+    cgpa : 8.4,
+    isPass : true,
+};
+
+const product = {
+    title : "Ball Pen",
+    rating : 4,
+    price : 270,
+    offer : 5,
+};
+
+const profile = {
+    fullName : "Asim Shrivastava",
+    post : 99,
+    followers : 1000,
+    following : 251,
+    company : "google",
+    isFollow : true
+};
+
+console.log(profile);
+
+student["name"] = "Ayushi Sharma";
+student["age"] = student["age"] + 1 ;
+
+console.log(student.age);
+let isFollow = true;
+console.log(isFollow);
+
+const number = 5;
+console.log(number);
